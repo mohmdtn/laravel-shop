a2451ed02f981427eecd5f62d8543a7cfcb5b6f1
unknown <mohammad.sawdegh@gamil.com>
2021-12-29 18:31:34 +0330

    add jetstream

--- shop-project/routes/web.php
+++ shop-project/routes/web.php
@@ -310,3 +310,7 @@ Route::prefix("admin")->namespace("App\Http\Controllers\Admin")->group(function 
     });
 
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
